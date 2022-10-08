293404fc7d558fc7be70646ab6472bb234faa901
amanyevge <yevgeaman@gmail.com>
2022-10-08 13:35:24 +0530

    commit

--- sales/models.py
+++ sales/models.py
@@ -1,9 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
-
 
 
 class Sales_Agent(models.Model):
@@ -23,10 +23,16 @@ class sale(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
-
-
-
+class Contactus(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    subject = models.CharField(max_length=20)
+    message = models.CharField(max_length=100)
     
+    def __str__(self):
+        return self.name
+
+
 
 
 
